f77309d0b7c4679f8047030d599d1f53b48986d7
MarsChang <mars09250148@yahoo.com.tw>
2018-01-30 10:03:09 +0800

    Initial commit

---
+++ app/src/main/java/com/mars/android20180130_1/MainActivity.java
@@ -0,0 +1,13 @@
+package com.mars.android20180130_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//定位以及google地圖
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
